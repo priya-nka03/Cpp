88d83431223ed8dfd65e6f70a7f7442f10fe4417
priya-nka03 <priyankapal5035@gmail.com>
2025-10-13 20:43:21 +0530

    operators

--- Operators.cpp
+++ Operators.cpp
@@ -42,5 +42,39 @@ Types
     cout<<"after pre decrement"<<a<<endl;
 
     // Relational Operator : used to define relation(comparison) between operands
-    
+    // ==,<=,>=,<,>,=! 
+    // they return a bool value true or false 0 or 1 0=false 1=true
+    int op1,op2,op3;
+    op1=34;
+    op2=90;
+    op3=333;
+    cout<<(op1>op2)<<endl;
+    cout<<('a'<'A')<<endl ;
+
+    //Logical Operator => used to combine two conditions and produce a bool output
+    // && - Logical AND gate (jb dono true honge tbhi true return karega)
+    // || - Logical OR gate (jb dono m se koi bhi true hoga then wo true return karega)
+    // ! - Not gate (opposite of the result obtained)
+
+    int num1,num2;
+    num1=0;num2=2;
+    cout<<(num1&&num2)<<endl;               // 0 beacause num1=0(flase) and num=2(ture)
+    cout<<(num1||num2)<<endl;
+    cout<<!(5<2)<<endl;
+
+
+    //Bitwise Operator :
+    int num3=4;
+    int num4=6;
+    cout<<(num3&num4)<<endl;          // bitwise AND  => return 1 when both are true
+    cout<<(num3|num4)<<endl;          // bitwise OR   => return 1 when any one of the input is one 
+    cout<<(~num3)<<endl;           // bitwise NOT => 1. binary m convert 2.NOT of that 3. ek negative number aaega to print that 2's complement of that
+    cout<<(num3^num4)<<endl;          // bitwise XOR
+
+
+    // Right Shift (>>)
+    cout<<(53>>1)<<endl; 
+    cout<<(53>>2)<<endl; 
+    //Left Shift (>>)
+    cout<<(53<<1)<<endl;
 }
