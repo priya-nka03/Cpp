3bf8904ceff939f81828503a2ee701a863012006
priya-nka03 <priyankapal5035@gmail.com>
2025-10-14 11:17:11 +0530

    if -else completed

--- control_flow_statements.cpp
+++ control_flow_statements.cpp
@@ -22,34 +22,42 @@ int main (){
     40-50= F
     less than 40 = Fail */
 
-    float marks;
-    cout<<"Enter the marks of the student:";
-    cin>>marks;
-    cout<<marks<<endl;
-    if(marks>90 and marks<=100){
-        cout<<"GRADE : A"<<endl<<"Remarks :Excellent! "<<endl;
-    }
-    else if (marks>80 and marks<=90){
-        cout<<"GRADE : B"<<endl<<"Remarks :Very Good"<<endl;
-    }
-    else if (marks>70 and marks<=80){
-        cout<<" GRADE : C"<<endl<<"Remarks :Good"<<endl;
-    }
-    else if (marks>60 and marks<=70){
-        cout<<"GRADE : D"<<endl<<"Remarks :Can do better"<<endl; 
-    }
-    else if (marks>50 and marks<=60){
-        cout<<"GRADE : E"<<endl;
-    }
-    else if (marks>40 and marks<=50){
-        cout<<"GRADE : F"<<endl;
-    }
-    else{
-        cout<<"FAIL"<<endl;
-    }
+    // float marks;
+    // cout<<"Enter the marks of the student:";
+    // cin>>marks;
+    // cout<<marks<<endl;
+    // if(marks>90 and marks<=100){
+    //     cout<<"GRADE : A"<<endl<<"Remarks :Excellent! "<<endl;
+    // }
+    // else if (marks>80 and marks<=90){
+    //     cout<<"GRADE : B"<<endl<<"Remarks :Very Good"<<endl;
+    // }
+    // else if (marks>70 and marks<=80){
+    //     cout<<" GRADE : C"<<endl<<"Remarks :Good"<<endl;
+    // }
+    // else if (marks>60 and marks<=70){
+    //     cout<<"GRADE : D"<<endl<<"Remarks :Can do better"<<endl; 
+    // }
+    // else if (marks>50 and marks<=60){
+    //     cout<<"GRADE : E"<<endl;
+    // }
+    // else if (marks>40 and marks<=50){
+    //     cout<<"GRADE : F"<<endl;
+    // }
+    // else{
+    //     cout<<"FAIL"<<endl;
+    // }
 
 
     // question 2: identify if the given year is leap year or not 
     int year ;
-    
+    cout<<"Enter year:";
+    cin>>year;
+    cout<<year<<endl;
+    if ((year%4==0 and year%100!=0) or year%400==0){
+        cout<<"Leap Year"<<endl;
+    }
+    else{
+        cout<<"Not a leap year"<<endl;
+    }
 }
