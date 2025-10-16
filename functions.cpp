d2bab01b703b21599c8f585fb5bb5f0f39160ece
priya-nka03 <priyankapal5035@gmail.com>
2025-10-16 20:30:49 +0530

    function theory completed

---
+++ functions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    /*Function : It is a block of code which is used to perform some specific task when it is called.
+    The idea is to put some commonly used or repeatdly used code inside a block (called function) and call the block when needed instead of writing the code again and again.
+    
+    main() is also a function which can be called only once whereas other functions can be called multiple times.
+    
+    return mark as the end of the function(main funciton).
+    */
+}
